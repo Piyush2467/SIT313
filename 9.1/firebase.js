3b717a650e8fd719f164bb259172cb14ead1831a
Piyush2467 <piyush4854.se24@chitkara.edu.in>
2025-10-29 00:42:33 +0530

    Update firebase.js

--- 9.1/firebase.js
+++ 9.1/firebase.js
@@ -1,22 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8loaHDQlMek29HNLvOzeI-VF7EuyUJF4",
-  authDomain: "task-7-c8704.firebaseapp.com",
-  projectId: "task-7-c8704",
-  storageBucket: "task-7-c8704.firebasestorage.app",
-  messagingSenderId: "421318253693",
-  appId: "1:421318253693:web:512516ffc590e81990a514",
-  measurementId: "G-2C68PKN8B1"
+  apiKey: "AIzaSyChTrVpn8-4eNkU44cMSYfXlHRtfJqik6o",
+  authDomain: "task9-6224a.firebaseapp.com",
+  projectId: "task9-6224a",
+  storageBucket: "task9-6224a.firebasestorage.app",
+  messagingSenderId: "817438347331",
+  appId: "1:817438347331:web:72aa0803a9fe6f716084f6",
+  measurementId: "G-JBYP70N4LP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize services
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
