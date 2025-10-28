a7fc452e68375e1443f33e8a39c5f18ec5ecabd6
Piyush2467 <piyush4854.se24@chitkara.edu.in>
2025-10-28 22:43:04 +0530

    Update firebase.js

--- 9.1/firebase.js
+++ 9.1/firebase.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoAKcutcnU_ZF7uNUSGaORnyNzlBtpO70",
-  authDomain: "task-week-7.firebaseapp.com",
+  apiKey: "AIzaSyBoAKcutcnU_ZF7uNGaORnyNzlBtpO70",
+  authDomain: "task-wek-7.firebseapp.com",
   projectId: "task-week-7",
-  storageBucket: "task-week-7.firebasestorage.app",
-  messagingSenderId: "305874154057",
-  appId: "1:305874154057:web:6b70fb7152a47c25394598"
+  storageBucket: "task-week-7.firebasestage.app",
+  messagingSenderId: "30587415407",
+  appId: "1:305874154057:web:60fb7152a47c25394598"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
